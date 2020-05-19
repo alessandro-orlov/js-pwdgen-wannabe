ba129d516ff65f81d1d3b394d3a16a8b6d05b8fa
Alessandro <alex.ixs@hotmail.com>
2020-05-19 16:36:53 +0200

    changed var names in JS

--- js/myscript.js
+++ js/myscript.js
@@ -4,10 +4,10 @@ console.log(userName);
 var userSurname = prompt('What\'s your surname');
 console.log(userSurname);
 
-var fvrtColor = prompt('What\'s your favorite color?');
+var favouriteColor = prompt('What\'s your favorite color?');
 console.log(fvrtColor)
 
 var currentYear = 20;
 
-var newPsswd = userName + userSurname + fvrtColor + currentYear
-document.getElementById('js-pswd').innerHTML = newPsswd;
+var newPassword = userName + userSurname + favouriteColor + currentYear
+document.getElementById('js-pswd').innerHTML = newPassword;
